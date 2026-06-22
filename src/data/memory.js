43dd9d65e934e8ef03a6d56bc22de412b2f5505f
Joao Piccini <joaopedropiccini@gmail.com>
2026-06-22 19:16:32 -0300

    Aula TODO's

---
+++ src/data/memory.js
@@ -0,0 +1,9 @@
+const todos = [
+    { id: 1, titulo: "Estudar Node.js", descricao: "Tarefa 1", feito: false },
+    { id: 2, titulo: "Fazer exercícios", descricao: "Tarefa 2", feito: true },
+    { id: 3, titulo: "Revisar código", descricao: "Tarefa 3", feito: false }
+];
+
+module.exports = {
+    todos
+}
